b6ab11a3753e049ee1715cda2af3fa629062e283
Dark <dark.dmake@gmail.com>
2019-04-26 14:25:01 +0300

    допилил расчет средней зп...

--- main.py
+++ main.py
@@ -2,12 +2,53 @@ import requests
 
 
 langs=['Python','PHP','Java','JavaScript','Ruby','C++','Objective-C','Swift','Go','C#']
-number_found={}
-for lang in langs:
-    params={'text':f'программист {lang}','area':1,'period':1}
+
+
+def predict_rub_salary(url):
+   
+    
+    salars=[]
+    
+    for num_vac in range(0,len(url.json()['items'])):
+
+        salary_from=url.json()['items'][num_vac]['salary']['from']
+        salary_to=url.json()['items'][num_vac]['salary']['to']
+        
+        salary = 0
+        
+        if url.json()['items'][num_vac]['salary']['currency'] == 'RUR':
+                      
+            if salary_from is None:
+                salary=salary_to*1.2
+            elif salary_to is None:
+                salary=salary_from*0.8
+            else :
+                salary=salary_from+salary_to
+            
+            salars.append(salary)
+
+    average_salary=sum(salars)/len(salars)
+
+
+
+    return int(average_salary)
+    
+        
+
+
+
+
+def main(vacansy):
+
+    params={'text':f'программист {vacansy}','area':1,'period':30,'only_with_salary':'True','currency':'RUR'}
     url= requests.get('https://api.hh.ru/vacancies',params=params)
-      
-    number_found[lang] = url.json()['found']
+    a=predict_rub_salary(url)
+    return a
+    vacancies_info={}
+    
 
 
-print(number_found)
+
+
+a=main(langs[0])
+print (a)
