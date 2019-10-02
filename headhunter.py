fe1737afea34d0913f181fbcd27b573f963e0ff5
Dark_Dmake <dark.dmake@gmail.com>
2019-10-02 21:40:53 +0300

    расчет средней зарплаты

--- headhunter.py
+++ headhunter.py
@@ -1,26 +1,43 @@
 import requests
 
 
-def searh_vacansy():
 
-    langs = ['Python', 'PHP', 'Java', 'JavaScript',
-             'Ruby', 'C++', 'Objective-C', 'Swift', 'Go', 'C#']
 
-    number_of_vacancies={}
-    for lang in langs:
+def predict_rub_salary(vacancy):
+    params={
+                "text":f"программист {vacancy}",
+                "area":'1',
+                "date_from":"2019-09-01",
+                "date_to":"2019-10-01",
+                "currency":"RUR",
 
-        params={
-            "text":f"программист{lang}",
-            "area":'1',
-            "date_from":"2019-09-01",
-            "date_to":"2019-10-01"
-                }
+            }
+    response=requests.get('https://api.hh.ru/vacancies',params=params)
 
-        response=requests.get('https://api.hh.ru/vacancies',params=params)
-        number_of_vacancies[lang]=response.json()["found"]
+    
 
-    return number_of_vacancies
+    for vacancy in response.json()['items']:
+        
+        if vacancy['salary'] and vacancy['salary']['currency'] =='RUR':
+            
+            if vacancy['salary']['from'] is None:
+                
+                yield int(vacancy['salary']['to'])*0.8
+
+            elif vacancy['salary']['to']  is None:
+                
+                yield int(vacancy['salary']['from'])*1.2
+            else:
+                yield (int(vacancy['salary']['to']) +  int(vacancy['salary']['from']))/2
+        else:
+            yield None
+             
+    
         
 
+g=predict_rub_salary('Python')
+for sg in g:
+    print(sg)
 
-print(searh_vacansy())
+# langs = ['Python', 'PHP', 'Java', 'JavaScript',
+    #          'Ruby', 'C++', 'Objective-C', 'Swift', 'Go', 'C#']
